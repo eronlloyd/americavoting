c39517b624b06630199586f447d14e51d07c77a9
Eron Lloyd <eronlloyd@gmail.com>
2015-12-16 18:50:16 -0500

    Improve admin model for Division

--- americavoting/core/admin.py
+++ americavoting/core/admin.py
@@ -6,4 +6,5 @@ from .models import Division
 @admin.register(Division)
 class DivisionAdmin(admin.ModelAdmin):
     search_fields = ["name", "abbreviation"]
+    list_display = ["name", "capital_city"]
     pass
